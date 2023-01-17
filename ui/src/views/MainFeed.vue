2125039bac69ed4eed6436d377a3363f8ff1cb75
debuqer <bagher2g@gmail.com>
2023-01-17 23:22:01 +0330

    add sidebar menu

--- ui/src/views/MainFeed.vue
+++ ui/src/views/MainFeed.vue
@@ -1,10 +1,15 @@
 <template>
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
   <div class="container bootstrap snippets bootdeys">
-    <div class="col-md-7 col-md-offset-2">
-      <PostSomething></PostSomething>
-      <div class="panel panel-body">
-        <TimeLine></TimeLine>
+    <div class="row">
+      <div class="col-md-3">
+        <SideBar></SideBar>
+      </div>
+      <div class="col-md-7 col-md-offset-2">
+        <PostSomething></PostSomething>
+        <div class="panel panel-body">
+          <TimeLine></TimeLine>
+        </div>
       </div>
     </div>
   </div>
@@ -13,11 +18,13 @@
 <script>
 import PostSomething from '@/components/PostSomething.vue'
 import TimeLine from '@/components/TimeLine.vue'
+import SideBar from '@/components/SideBar.vue'
 export default {
   name: 'MainFeed',
   components: {
     TimeLine,
-    PostSomething
+    PostSomething,
+    SideBar
   }
 }
 </script>
