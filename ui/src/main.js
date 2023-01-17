8efbd18799f088b605ac625bcd6b00773ca88f09
debuqer <bagher2g@gmail.com>
2023-01-17 22:00:05 +0330

    create feed page

--- ui/src/main.js
+++ ui/src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'jquery/src/jquery.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 createApp(App).use(store).use(router).mount('#app')
