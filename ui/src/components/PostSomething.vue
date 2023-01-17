2d96ef0bea402fe2117bdd3f4aa81e15c43324f3
debuqer <bagher2g@gmail.com>
2023-01-17 23:11:55 +0330

    add PostSomething Button

---
+++ ui/src/components/PostSomething.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="panel">
+    <div class="panel-body">
+      <textarea class="form-control" rows="2" placeholder="What are you thinking?"></textarea>
+      <div class="mar-top clearfix">
+        <button class="btn btn-sm btn-primary pull-right" type="submit"><i class="fa fa-pencil fa-fw"></i>
+          Share</button>
+        <a class="btn btn-trans btn-icon fa fa-video-camera add-tooltip" href="#" data-original-title="Add Video"
+          data-toggle="tooltip"></a>
+        <a class="btn btn-trans btn-icon fa fa-camera add-tooltip" href="#" data-original-title="Add Photo"
+          data-toggle="tooltip"></a>
+        <a class="btn btn-trans btn-icon fa fa-file add-tooltip" href="#" data-original-title="Add File"
+          data-toggle="tooltip"></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostSomething',
+  props: {
+  }
+}
+</script>
--- ui/src/views/MainFeed.vue
+++ ui/src/views/MainFeed.vue
@@ -2,21 +2,7 @@
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
   <div class="container bootstrap snippets bootdeys">
     <div class="col-md-7 col-md-offset-2">
-      <div class="panel">
-        <div class="panel-body">
-          <textarea class="form-control" rows="2" placeholder="What are you thinking?"></textarea>
-          <div class="mar-top clearfix">
-            <button class="btn btn-sm btn-primary pull-right" type="submit"><i class="fa fa-pencil fa-fw"></i>
-              Share</button>
-            <a class="btn btn-trans btn-icon fa fa-video-camera add-tooltip" href="#" data-original-title="Add Video"
-              data-toggle="tooltip"></a>
-            <a class="btn btn-trans btn-icon fa fa-camera add-tooltip" href="#" data-original-title="Add Photo"
-              data-toggle="tooltip"></a>
-            <a class="btn btn-trans btn-icon fa fa-file add-tooltip" href="#" data-original-title="Add File"
-              data-toggle="tooltip"></a>
-          </div>
-        </div>
-      </div>
+      <PostSomething></PostSomething>
       <div class="panel panel-body">
         <TimeLine></TimeLine>
       </div>
@@ -25,11 +11,13 @@
 </template>
 
 <script>
+import PostSomething from '@/components/PostSomething.vue'
 import TimeLine from '@/components/TimeLine.vue'
 export default {
   name: 'MainFeed',
   components: {
-    TimeLine
+    TimeLine,
+    PostSomething
   }
 }
 </script>
