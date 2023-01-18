12e7e607c2093dc1aec254b86c6beee34dfb7194
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-01-18 14:47:17 +0330

    add likes and comment

--- ui/src/components/TimeLineEntry.vue
+++ ui/src/components/TimeLineEntry.vue
@@ -3,12 +3,21 @@
     <div class="timeline-stat">
       <div class="timeline-icon"><img :src="item.profile" alt="Profile picture">
       </div>
-      <div class="timeline-time">{{ item.time }}</div>
+      <div class="timeline-time"><a href="#" class="btn-link text-semibold">{{ item.author }}</a><br/>{{ item.time }}</div>
     </div>
     <div class="timeline-label">
-      <p class="mar-no pad-btm"><a href="#" class="btn-link text-semibold">{{ item.author }}.</a> commented on <a href="#">{{ item.label }}</a>
+      <p class="mar-no pad-btm"> {{ item.description ?? 'commented on' }} when <a href="#">{{ item.label }}</a>
       </p>
+      <div v-if="item.image" >
+        <img class="img-fluid" :src="item.image">
+      </div>
       <blockquote class="bq-sm bq-open">{{ item.message }}</blockquote>
+      <div>
+        <div class="activity__list__footer">
+          <a href="#"> <i class="fa fa-thumbs-up"></i>{{ item.likes }}</a>
+          <a href="#"> <i class="fa fa-comments"></i>{{ item.comments }}</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,3 +30,36 @@ export default {
   }
 }
 </script>
+
+<style>
+.activity__list__footer {
+    display: -ms-flexbox;
+    display: -webkit-box;
+    display: flex;
+    margin-top: 23px;
+    margin-left: 43px;
+    padding: 13px 8px 0;
+    color: #999;
+    border-top: 1px dotted #ccc;
+}
+
+.activity__list__footer a {
+    color: inherit;
+}
+
+.activity__list__footer a + a {
+    margin-left: 15px;
+}
+
+.activity__list__footer i {
+    margin-right: 8px;
+}
+
+.activity__list__footer a:hover {
+    color: #222;
+}
+
+.activity__list__footer span {
+    margin-left: auto;
+}
+</style>
--- ui/src/views/MainFeed.vue
+++ ui/src/views/MainFeed.vue
@@ -32,18 +32,25 @@ export default {
         {
           id: 1,
           profile: 'https://bootdey.com/img/Content/avatar/avatar6.png',
-          label: '22:53',
+          label: 'Time was 22:53',
           author: 'Bagher',
           time: '1 Hour ago',
-          message: 'This is cold'
+          description: 'USA attacked to China',
+          message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          image: 'https://bootdey.com/img/Content/avatar/avatar6.png',
+          likes: 0,
+          comments: 12
         },
         {
           id: 2,
           profile: 'https://bootdey.com/img/Content/avatar/avatar6.png',
-          label: '22:53',
+          label: 'Time was 22:53',
           author: 'Bagher',
           time: '1 Hour ago',
-          message: 'This is cold'
+          description: 'China Attacked to Spain',
+          message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          likes: 2,
+          comments: 12
         }
       ]
     }
