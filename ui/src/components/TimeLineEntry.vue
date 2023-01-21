1459dd63fc3f68d627c5cffaf5307cb1c547c09d
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-01-21 15:29:55 +0330

    like and sendComment

--- ui/src/components/TimeLineEntry.vue
+++ ui/src/components/TimeLineEntry.vue
@@ -30,28 +30,20 @@
         </div>
         <div class="stats">
           <span class="fa-stack fa-fw stats-icon">
-          <i class="fa fa-circle fa-stack-2x text-danger"></i>
-          <i class="fa fa-heart fa-stack-1x fa-inverse t-plus-1"></i>
-          </span>
-          <span class="fa-stack fa-fw stats-icon">
           <i class="fa fa-circle fa-stack-2x text-primary"></i>
-          <i class="fa fa-thumbs-up fa-stack-1x fa-inverse"></i>
+          <i class="fa fa-thumbs-up fa-stack-1x fa-inverse" @click="like"></i>
           </span>
-          <span class="stats-total">4.3k</span>
+          <span class="stats-total">origin</span>
         </div>
-    </div>
-    <div class="timeline-footer">
-        <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-thumbs-up fa-fw fa-lg m-r-3"></i> Like</a>
-        <a href="javascript:;" class="m-r-15 text-inverse-lighter"><i class="fa fa-comments fa-fw fa-lg m-r-3"></i> Comment</a>
     </div>
     <div class="timeline-comment-box">
         <div class="user"><img src="https://bootdey.com/img/Content/avatar/avatar6.png"></div>
         <div class="input">
           <form action="">
               <div class="input-group">
-                <input type="text" class="form-control rounded-corner" placeholder="Write a comment...">
+                <input type="text" class="form-control rounded-corner" placeholder="Write a comment..." v-model="comment">
                 <span class="input-group-btn p-l-10">
-                <button class="btn btn-primary f-s-12 rounded-corner" type="button">Comment</button>
+                <button class="btn btn-primary f-s-12 rounded-corner" type="button" @click="sendComment">Comment</button>
                 </span>
               </div>
           </form>
@@ -65,6 +57,20 @@ export default {
   name: 'TimeLineEntry',
   props: {
     item: Object
+  },
+  data: function () {
+    return {
+      comment: ''
+    }
+  },
+  methods: {
+    like: function () {
+      console.log(this.item.id)
+    },
+    sendComment: function () {
+      console.log(this.comment)
+      this.comment = ''
+    }
   }
 }
 </script>
