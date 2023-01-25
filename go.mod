b7a881e7f84dd7739dd794b5f38a27f22fa4d64a
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-01-25 14:24:24 +0330

    define first route

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/debuqer/microblog
 
 go 1.19
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
 
 func main() {
-	fmt.Println("Hello")
+	router := httprouter.New()
+	router.GET("/", hello)
+
+	http.ListenAndServe(":8080", router)
 }
