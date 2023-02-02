b0da55f49d07b719eddff6d409ac990831269c3b
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-02-02 13:15:03 +0330

    use short name

--- routes/api.go
+++ routes/api.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/debuqer/microblog/controllers"
+	c "github.com/debuqer/microblog/controllers"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -13,51 +13,51 @@ func ApiRouter() *httprouter.Router {
 
 	// Tags
 	// get tags
-	router.GET("/tag/all", controllers.TagAll)
+	router.GET("/tag/all", c.TagAll)
 	// store
-	router.POST("/tag/store", controllers.TagStore)
+	router.POST("/tag/store", c.TagStore)
 	// show tag
-	router.GET("/tag/:tgid", controllers.TagShow)
+	router.GET("/tag/:tgid", c.TagShow)
 
 	/** Timeline **/
 	// show timeline
-	router.GET("/timeline/:slug", controllers.TimelineShow)
+	router.GET("/timeline/:slug", c.TimelineShow)
 	// store
-	router.POST("/timeline/store", controllers.TimelineStore)
+	router.POST("/timeline/store", c.TimelineStore)
 	// update
-	router.PUT("/timeline/update", controllers.TimelineUpdate)
+	router.PUT("/timeline/update", c.TimelineUpdate)
 	// clone into
-	router.POST("/timeline/clone", controllers.TimelineClone)
+	router.POST("/timeline/clone", c.TimelineClone)
 	// merge
-	router.PUT("/timeline/merge", controllers.TimelineMerge)
+	router.PUT("/timeline/merge", c.TimelineMerge)
 	// star
-	router.POST("/timeline/reaction", controllers.TimelineReaction)
+	router.POST("/timeline/reaction", c.TimelineReaction)
 	// load events
-	router.GET("/timeline/events/:scale", controllers.TimelineEvents)
+	router.GET("/timeline/events/:scale", c.TimelineEvents)
 	// latest updated
-	router.GET("/timeline/latest", controllers.TimelineLatest)
+	router.GET("/timeline/latest", c.TimelineLatest)
 
 	/** Event **/
 	// show event
-	router.GET("/timeline/event/:eid", controllers.EventShow)
+	router.GET("/timeline/event/:eid", c.EventShow)
 	// store
-	router.POST("/timeline/event/store", controllers.EventStore)
+	router.POST("/timeline/event/store", c.EventStore)
 	// update
-	router.PUT("/timeline/event/update", controllers.EventUpdate)
+	router.PUT("/timeline/event/update", c.EventUpdate)
 	// clone into
-	router.POST("/timeline/event/clone", controllers.EventClone)
+	router.POST("/timeline/event/clone", c.EventClone)
 	// star
-	router.POST("/timeline/event/reaction", controllers.EventReaction)
+	router.POST("/timeline/event/reaction", c.EventReaction)
 	// comment
-	router.POST("/timeline/event/comment", controllers.EventComment)
+	router.POST("/timeline/event/comment", c.EventComment)
 	// load comments
-	router.GET("/timeline/event/comments", controllers.EventComments)
+	router.GET("/timeline/event/comments", c.EventComments)
 
 	/** User **/
 	// show user
-	router.GET("/user/:uid", controllers.UserShow)
+	router.GET("/user/:uid", c.UserShow)
 	// update user
-	router.PUT("/user/update", controllers.UserUpdate)
+	router.PUT("/user/update", c.UserUpdate)
 
 	/** Home **/
 	// sign up
@@ -65,11 +65,11 @@ func ApiRouter() *httprouter.Router {
 	// login
 	router.POST("/login", hello)
 	// forget password
-	router.GET("/forget-password", controllers.UserForgetPassword)
+	router.GET("/forget-password", c.UserForgetPassword)
 	// verify link
-	router.GET("/verify-link", controllers.UserVerifyLink)
+	router.GET("/verify-link", c.UserVerifyLink)
 	// update password
-	router.POST("/update-password", controllers.UserUpdatePassword)
+	router.POST("/update-password", c.UserUpdatePassword)
 
 	return router
 }
