ed9125d3586683b75155b6d48f5209ba870bf0bc
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-02-02 12:58:36 +0330

    api router route define

--- models/timeline.go
+++ models/timeline.go
@@ -1,6 +1,8 @@
 package models
 
 type Timeline struct {
+	Id           int
+	Slug         string
 	Label        string
 	Descriptiuon string
 	Tags         []Tag
--- routes/api.go
+++ routes/api.go
@@ -9,7 +9,66 @@ import (
 
 func ApiRouter() *httprouter.Router {
 	router := httprouter.New()
-	router.GET("/", hello)
+
+	// Tags
+	// get tags
+	router.GET("/tag/all", hello)
+	// store
+	router.POST("/tag/store", hello)
+	// show tag
+	router.GET("/tag/:tgid", hello)
+
+	/** Timeline **/
+	// show timeline
+	router.GET("/timeline/:slug", hello)
+	// store
+	router.POST("/timeline/store", hello)
+	// update
+	router.PUT("/timeline/update", hello)
+	// clone into
+	router.POST("/timeline/clone", hello)
+	// merge
+	router.PUT("/timeline/merge", hello)
+	// star
+	router.POST("/timeline/reaction", hello)
+	// load events
+	router.GET("/timeline/events/:scale", hello)
+	// latest updated
+	router.GET("/timeline/latest", hello)
+
+	/** Event **/
+	// show event
+	router.GET("/timeline/event/:eid", hello)
+	// store
+	router.POST("/timeline/event/store", hello)
+	// update
+	router.PUT("/timeline/event/update", hello)
+	// clone into
+	router.POST("/timeline/event/clone", hello)
+	// star
+	router.POST("/timeline/event/reaction", hello)
+	// comment
+	router.POST("/timeline/event/comment", hello)
+	// load comments
+	router.GET("/timeline/event/comments", hello)
+
+	/** User **/
+	// show user
+	router.GET("/user/:uid", hello)
+	// update user
+	router.PUT("/user/update", hello)
+
+	/** Home **/
+	// sign up
+	router.POST("/signup", hello)
+	// login
+	router.POST("/login", hello)
+	// forget password
+	router.GET("/forget-password", hello)
+	// verify link
+	router.GET("/verify-password", hello)
+	// update password
+	router.POST("/update-password", hello)
 
 	return router
 }
